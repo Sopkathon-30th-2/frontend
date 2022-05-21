c5b7893789ec3820a0b6ce26e6df897612e36e48
nahee-park <jessica396@ewhain.net>
2022-05-22 06:15:58 +0900

    feat: clean mail view 퍼블리싱

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,21 +1,32 @@
+import StyledComponents from 'styled-components';
 import styled from 'styled-components';
 import logo from '../../assets/image/logo.svg';
 
-const HeaderLogo = {
-  Logo: styled.img`
-    width: 148px;
-    height: 86.17px;
-    position: absolute;
-    align-items: center;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    margin-top: 85px;
-  `,
-};
-
 function Header() {
-  return <HeaderLogo.Logo src={logo} />;
+  return (
+    <HeaderLogo.Root>
+      <HeaderLogo.Logo src={logo} />
+    </HeaderLogo.Root>
+  );
 }
 
 export default Header;
+
+const HeaderLogo = {
+  Root: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  `,
+  Logo: styled.img`
+    width: 148px;
+    height: 86.17px;
+    /* position: absolute; */
+    /* align-items: center; */
+    /* top: 0;
+    left: 50%;
+    transform: translateX(-50%); */
+    margin-top: 85px;
+  `,
+};
