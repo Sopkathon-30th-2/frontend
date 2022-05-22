60e2eeaccf954b10fef046888900178913c4334a
nahee-park <jessica396@ewhain.net>
2022-05-22 09:02:38 +0900

    Revert "feat: loading"
    This reverts commit e71c41f163b6b1afa4ddeba32f7fee9ab167d774.

--- src/components/common/ErrorComponent.jsx
+++ src/components/common/ErrorComponent.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 
 function ErrorComponent({ error }) {
   console.log('>>>error', error);
-  return <Styled.Root>일시적인 네트워크 오류로 데이터를 불러올 수 없습니다.</Styled.Root>;
+  return <div>ErrorComponent</div>;
 }
 
 export default ErrorComponent;
-
-const Styled = {
-  Root: styled.div`
-    margin-top: 5rem;
-    font-size: 4rem;
-  `,
-};
--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 
 function Loading() {
-  return <Styled.Root>로딩중 . . . </Styled.Root>;
+  return <div>Loading</div>;
 }
 
 export default Loading;
-
-const Styled = {
-  Root: styled.div`
-    margin-top: 5rem;
-    font-size: 4rem;
-  `,
-};
